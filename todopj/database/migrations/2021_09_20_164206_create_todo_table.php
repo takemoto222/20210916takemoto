60ea31572b4185611b53ef0ac6ddbe3a23a3adb8
takemoto222 <omochi9797@icloud.com>
2021-09-20 17:11:29 +0900

    todo_mg

---
+++ todopj/database/migrations/2021_09_20_164206_create_todo_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todo', function (Blueprint $table) {
+            $table->id();
+            $table->string('content');
+            $table->timestamps('created_at')->useCurrent()->nullable();
+            $table->timestamps('updated_at')->useCurrent()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todo');
+    }
+}
