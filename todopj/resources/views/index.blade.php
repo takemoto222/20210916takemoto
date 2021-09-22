dd9b7b4498d6ef86c9d8528abb5cb47789cb53c2
takemoto222 <omochi9797@icloud.com>
2021-09-22 12:57:18 +0900

    todo_blade_4

--- todopj/resources/views/index.blade.php
+++ todopj/resources/views/index.blade.php
@@ -118,7 +118,7 @@ th {
 @section('content')
 
 
-
+<table>
   @foreach ($items as $item)
   <tr>
     <!--日付-->
@@ -140,7 +140,7 @@ th {
 
   </tr>
 @endforeach
-
+</table>
 
     </form>
 
---
+++ todopj/time.php
@@ -0,0 +1,3 @@
+<?php
+$date = new DateTime();
+echo $date->foreach('Y-m-d H:i:s');
